ced4f05f3c37c790a35690bbb9bdda9ee6f5e128
Dan Hughes <danhughes.dh+github@gmail.com>
2018-08-11 23:19:05 +0100

    Add deck and exp1

---
+++ components/IFrame.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default styled.iframe`
+	height: 90vh;
+	width: 100%;
+	border: none;
+	border-radius: 10px;
+	box-shadow: 5px 10px 20px rgba(0, 0, 0, 0.5);
+`;
---
+++ components/Notes.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export default styled.p`
+	text-align: left;
+	padding: 1em;
+	background-color: #EEE;
+	font-size: 90%;
+`;
